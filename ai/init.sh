134e6bf29ba6abe255aefc1c8e121496c0dac98e
Amol Yadav <amyssnipet@yahoo.com>
2025-09-14 00:10:44 +0530

    Update init.sh

--- ai/init.sh
+++ ai/init.sh
@@ -1,54 +1,82 @@
 #!/bin/bash
 
 set -e
+
+echo "=================================================="
 echo "Installing Ollama ..."
+echo "=================================================="
 curl -fsSl https://ollama.com/install.sh | sh
-echo "Installed Ollama"
-echo "Starting Ollama server..."
+echo "Ollama Installed Successfully"
+echo
+
+echo "=================================================="
+echo "Starting Ollama Server ..."
+echo "=================================================="
 ollama serve >/dev/null 2>&1 &
 sleep 5
 until curl -s http://127.0.0.1:11434/api/tags >/dev/null; do
   echo "Waiting for Ollama to be ready..."
   sleep 2
 done
-echo "Ollama server is ready!"
+echo "Ollama Server is Ready"
+echo
+
+echo "=================================================="
 echo "Downloading Chat Base Model Architecture ..."
+echo "=================================================="
 ollama run qwen3 --verbose <<< "exit"
-echo "Downloaded Chat Base Model Architecture"
+echo "Chat Base Model Architecture Downloaded"
+echo
+
+echo "=================================================="
 echo "Downloading Vision Model Architecture ..."
+echo "=================================================="
 ollama run qwen2.5vl --verbose <<< "exit"
-echo "Downloaded Vision Model Architecture"
+echo "Vision Model Architecture Downloaded"
+echo
+
+echo "=================================================="
 echo "Preparing Dataset for vivi LLM ..."
+echo "=================================================="
 cat > Modelfile <<EOF
 FROM qwen3
-
-# set the temperature to 1 [higher is more creative, lower is more coherent]
 PARAMETER temperature 1
-
-# set the system message
 SYSTEM """
 You are VIVI, a chatbot developed for VIVI Enterprises to serve various businesses by creator Amol Yadav.
 """
 EOF
-echo "Dataset Prepared" 
+echo "Dataset Prepared"
+echo
+
+echo "=================================================="
 echo "Training vivi LLM ..."
+echo "=================================================="
 ollama create vivi -f ./Modelfile
-echo "Model vivi Trained Successfully"
+echo "vivi Model Trained Successfully"
 rm Modelfile
+echo
+
+echo "=================================================="
 echo "Preparing Dataset for vivi-vl ..."
+echo "=================================================="
 cat > Modelfile <<EOF
 FROM qwen2.5vl
-
-# set the temperature to 1 [higher is more creative, lower is more coherent]
 PARAMETER temperature 1
-
-# set the system message
 SYSTEM """
 You are VIVI VL, a document parser model developed for VIVI Enterprises to serve various businesses by creator Amol Yadav.
 """
 EOF
-echo "Prepared Dataset"
+echo "Dataset Prepared"
+echo
+
+echo "=================================================="
 echo "Training vivi-vl LLM ..."
+echo "=================================================="
 ollama create vivi-vl -f ./Modelfile
-echo "Model vivi-vl Trained Successfully"
-echo "AI Infra setup Successfull!"
+echo "vivi-vl Model Trained Successfully"
+rm Modelfile
+echo
+
+echo "=================================================="
+echo "AI Infrastructure Setup Successful"
+echo "=================================================="
